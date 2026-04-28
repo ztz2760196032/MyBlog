0f9f04e2e1cad9a44540960d28b47ac7485ca91d
千清 <126779522+ztz2760196032@users.noreply.github.com>
2026-04-28 23:36:58 +0800

    Update custom.js

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,1 +1,10 @@
 // 这里编写自定义js脚本；将被静态引入到页面中
+// 爱心
+var s1 = document.createElement('script');
+s1.src = 'https://api.azpay.cn/729/1.js';
+document.head.appendChild(s1);
+
+// 烟花
+var s2 = document.createElement('script');
+s2.src = 'https://api.azpay.cn/729/2.js';
+document.head.appendChild(s2);
